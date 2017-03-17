e6b5ebcf72de5055964ec6439d54233e80c4f4f8
Gryffon <ryan@karetas.com>
2017-03-17 13:35:38 -0400

    Update for Python 3.4

--- example.py
+++ example.py
@@ -4,24 +4,24 @@ import asyncio
 client = discord.Client()
 
 @client.event
-async def on_ready():
+@asyncio.coroutine on_ready():
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
     print('------')
 
 @client.event
-async def on_message(message):
+@asyncio.coroutine on_message(message):
     if message.content.startswith('!test'):
         counter = 0
-        tmp = await client.send_message(message.channel, 'Calculating messages...')
+        tmp = yield from client.send_message(message.channel, 'Calculating messages...')
         async for log in client.logs_from(message.channel, limit=100):
             if log.author == message.author:
                 counter += 1
 
-        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
+        yield from client.edit_message(tmp, 'You have {} messages.'.format(counter))
     elif message.content.startswith('!sleep'):
-        await asyncio.sleep(5)
-        await client.send_message(message.channel, 'Done sleeping')
+        yield from asyncio.sleep(5)
+        yield from client.send_message(message.channel, 'Done sleeping')
 
 client.run('token')
