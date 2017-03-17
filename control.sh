9eb067accff64e483ee4743e397e10c5f6d4a7f6
Gryffon <ryan@karetas.com>
2017-03-17 17:52:08 -0400

    Have control script use supervisor

--- control.sh
+++ control.sh
@@ -1,2 +1,27 @@
-#Start SusumuTakuan
-python example.py
+#!/bin/bash
+
+#
+# control.sh
+#
+# A control script to run SusumuTakuan, 
+# the L5R DiscordBot
+#
+
+cmd=$1
+
+if [ "$cmd" eq "start" ]; then
+		#Start SusumuTakuan
+		supervisorctl start takuan
+
+elif [ "$cmd" eq "stop" ]; then
+		#Stop SusumuTakuan
+		supervisorctl start takuan
+
+elif [ "$cmd" eq "restart" ]; then
+		#Restart SusumuTakuan
+		supervisorctl restart takuan
+
+elif [ "$cmd" eq "refresh" ]; then
+		#Refresh Code
+		git pull
+fi
