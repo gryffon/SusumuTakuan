c30c749558ac4e04244870871ca9c6edffb2efe3
Gryffon <ryan@karetas.com>
2017-03-17 13:57:37 -0400

    Revert 3.4 changes
    Use Python 3.5 instead

--- example.py
+++ example.py
@@ -4,26 +4,24 @@ import asyncio
 client = discord.Client()
 
 @client.event
-@asyncio.coroutine 
-def on_ready():
+async def on_ready():
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
     print('------')
 
 @client.event
-@asyncio.coroutine 
-def on_message(message):
+async def on_message(message):
     if message.content.startswith('!test'):
         counter = 0
-        tmp = yield from client.send_message(message.channel, 'Calculating messages...')
+        tmp = await client.send_message(message.channel, 'Calculating messages...')
         async for log in client.logs_from(message.channel, limit=100):
             if log.author == message.author:
                 counter += 1
 
-        yield from client.edit_message(tmp, 'You have {} messages.'.format(counter))
+        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
     elif message.content.startswith('!sleep'):
-        yield from asyncio.sleep(5)
-        yield from client.send_message(message.channel, 'Done sleeping')
+        await asyncio.sleep(5)
+        await client.send_message(message.channel, 'Done sleeping')
 
 client.run('token')
