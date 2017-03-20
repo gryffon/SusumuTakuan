1a5b7f5ccc549166619e0e56df5d83e3d5f78037
Gryffon <ryan@karetas.com>
2017-03-20 15:21:03 -0400

    Refactor developer functions

---
+++ developer.py
@@ -0,0 +1,80 @@
+#
+# developer.py
+#
+# developer functions for managing SusumuTakuan remotely
+#
+
+import discord
+import asyncio
+import subprocesss
+
+async def update_git(client, message):
+    tmp = await client.send_message(message.channel, 'Updating my code via git...')
+	users = message.channel.recipients
+	for user in users:
+		if user.id != client.user.id:
+			print('%s/%s requested to update my code.' % (user.name, user.id))
+
+		if user.id in config.developers:
+			process = subprocess.run(["sh", "control.sh", "refresh"], universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+			tmp = await client.send_message(message.channel, process.stdout)
+		else:
+			print('%s/%s not allowed to run update command.' % (user.name, user.id))
+			tmp = await client.send_message(message.channel, 'Unauthorized')	
+
+
+async def restart_bot(client, message):
+	tmp = await client.send_message(message.channel, 'Restarting myself...')
+	users = message.channel.recipients
+	for user in users:
+		if user.id != client.user.id:
+			print('%s/%s requested to restart me.' % (user.name, user.id))
+
+		if user.id in config.developers:
+			process = subprocess.run(["sh", "control.sh", "restart"], universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+			tmp = await client.send_message(message.channel, process.stdout)
+		else:
+			print('%s/%s not allowed to run restart command.' % (user.name, user.id))
+			tmp = await client.send_message(message.channel, 'Unauthorized')
+        
+async def debug_output(client, message):
+	tmp = await client.send_message(message.channel, 'Providing debug log of stdout...')
+	message_array=message.content.split(" ")
+	try:
+		num_lines=int(message_array[1])
+	except ValueError:
+		print("debug_error: User gave invalid value for number of lines")
+		tmp = await client.send_message(message.channel, '%s is not a valid number of lines' % (message_array[1]))
+	log_lines='-%d' % (num_lines)
+	users = message.channel.recipients
+	for user in users:
+		if user.id != client.user.id:
+			print('%s/%s requested output log.' % (user.name, user.id))
+
+		if user.id in config.developers:
+			process = subprocess.run(["tail", log_lines, "logs/output.log"], universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+			tmp = await client.send_message(message.channel, process.stdout)
+		else:
+			print('%s/%s not allowed to run debug command.' % (user.name, user.id))
+			tmp = await client.send_message(message.channel, 'Unauthorized')
+
+async def debug_error(client, message):
+	tmp = await client.send_message(message.channel, 'Providing debug log of stderr...')
+	message_array=message.content.split(" ")
+	try:
+		num_lines=int(message_array[1])
+	except ValueError:
+		print("debug_error: User gave invalid value for number of lines")
+		tmp = await client.send_message(message.channel, '%s is not a valid number of lines' % (message_array[1]))
+	log_lines='-%d' % (num_lines)
+	users = message.channel.recipients
+	for user in users:
+		if user.id != client.user.id:
+			print('%s/%s requested error log.' % (user.name, user.id))
+
+		if user.id in config.developers:
+			process = subprocess.run(["tail", log_lines, "logs/error.log"], universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+			tmp = await client.send_message(message.channel, process.stdout)
+		else:
+			print('%s/%s not allowed to run debug command.' % (user.name, user.id))
+			tmp = await client.send_message(message.channel, 'Unauthorized')
--- susumutakuan.py
+++ susumutakuan.py
@@ -5,6 +5,7 @@ import signal
 import sys
 import subprocess
 import imp
+import developer
 
 #Set up Client State
 CLIENT_TOKEN=os.environ['TOKEN']
@@ -42,73 +43,13 @@ async def on_message(message):
     #Look at DMs for special commands
     if message.channel.type == discord.ChannelType.private:
         if message.content.startswith('!update'):
-            tmp = await client.send_message(message.channel, 'Updating my code via git...')
-            users = message.channel.recipients
-            for user in users:
-                if user.id != client.user.id:
-                    print('%s/%s requested to update my code.' % (user.name, user.id))
-
-                if user.id in config.developers:
-                    process = subprocess.run(["sh", "control.sh", "refresh"], universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-                    tmp = await client.send_message(message.channel, process.stdout)
-                else:
-                    print('%s/%s not allowed to run update command.' % (user.name, user.id))
-                    tmp = await client.send_message(message.channel, 'Unauthorized')
+            developer.update_git(client, message)
         elif message.content.startswith('!restart'):
-            tmp = await client.send_message(message.channel, 'Restarting myself...')
-            users = message.channel.recipients
-            for user in users:
-                if user.id != client.user.id:
-                    print('%s/%s requested to restart me.' % (user.name, user.id))
-
-                if user.id in config.developers:
-                    process = subprocess.run(["sh", "control.sh", "restart"], universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-                    tmp = await client.send_message(message.channel, process.stdout)
-                else:
-                    print('%s/%s not allowed to run restart command.' % (user.name, user.id))
-                    tmp = await client.send_message(message.channel, 'Unauthorized')
+            developer.restart_bot(client, message)
         elif message.content.startswith('!debug_output'):
-            tmp = await client.send_message(message.channel, 'Providing debug log of stdout...')
-            message_array=message.content.split(" ")
-            try:
-                 num_lines=int(message_array[1])
-            except ValueError:
-                print("debug_error: User gave invalid value for number of lines")
-                tmp = await client.send_message(message.channel, '%s is not a valid number of lines' % (message_array[1]))
-            log_lines='-%d' % (num_lines)
-            users = message.channel.recipients
-            for user in users:
-                if user.id != client.user.id:
-                    print('%s/%s requested output log.' % (user.name, user.id))
-
-                if user.id in config.developers:
-                    process = subprocess.run(["tail", log_lines, "logs/output.log"], universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-                    tmp = await client.send_message(message.channel, process.stdout)
-                else:
-                    print('%s/%s not allowed to run debug command.' % (user.name, user.id))
-                    tmp = await client.send_message(message.channel, 'Unauthorized')
+            developer.debug_output(client, message)
         elif message.content.startswith('!debug_error'):
-            tmp = await client.send_message(message.channel, 'Providing debug log of stderr...')
-            message_array=message.content.split(" ")
-            try:
-                num_lines=int(message_array[1])
-            except ValueError:
-                print("debug_error: User gave invalid value for number of lines")
-                tmp = await client.send_message(message.channel, '%s is not a valid number of lines' % (message_array[1]))
-            log_lines='-%d' % (num_lines)
-            users = message.channel.recipients
-            for user in users:
-                if user.id != client.user.id:
-                    print('%s/%s requested error log.' % (user.name, user.id))
-
-                if user.id in config.developers:
-                    process = subprocess.run(["tail", log_lines, "logs/error.log"], universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-                    tmp = await client.send_message(message.channel, process.stdout)
-                else:
-                    print('%s/%s not allowed to run debug command.' % (user.name, user.id))
-                    tmp = await client.send_message(message.channel, 'Unauthorized')
-
-
+            developer.debug_error(client, message)
 
     if message.content.startswith('!test'):
         counter = 0
