4a2ee25502901e76f2d98ebe95e44be9e827ee5f
Gryffon <ryan@karetas.com>
2017-05-02 17:11:18 -0400

    Fix relationships...again

--- database.py
+++ database.py
@@ -36,13 +36,41 @@ class Channel(Base):
 	id = Column(Integer, primary_key=True)
 	name = Column(String(250), nullable=False)
 	server_id = Column(Integer, ForeignKey('servers.id'))
-	server = relationship(Server, backref=backref('roles', uselist=True))
+	server = relationship(Server, backref=backref('channels', uselist=True))
 	squelch = Column(Boolean, nullable=False)
 
 class CommandClass(Base):
 	__tablename__ = 'commandclasses'
 	id = Column(Integer, primary_key=True)
 	name = Column(String(250), nullable=False)
+
+class RoleCommandAccess(Base):
+	__tablename__ = 'rolecommands'
+	id = Column(Integer, primary_key=True)
+	role_id = Column(Integer, ForeignKey('roles.id'))
+	role = relationship(Role, backref=backref('rolecommands', uselist=True))
+	squelch = Column(Boolean, nullable=False)
+
+class RoleCommandClassAccess(Base):
+	__tablename__ = 'rolecommandclasses'
+	id = Column(Integer, primary_key=True)
+	role_id = Column(Integer, ForeignKey('roles.id'))
+	role = relationship(Role, backref=backref('rolecommandclasses', uselist=True))
+	squelch = Column(Boolean, nullable=False)
+
+class UserCommandAccess(Base):
+	__tablename__ = 'usercommands'
+	id = Column(Integer, primary_key=True)
+	user_id = Column(Integer, ForeignKey('users.id'))
+	user = relationship(User, backref=backref('usercommands', uselist=True))
+	squelch = Column(Boolean, nullable=False)
+
+class UserCommandClassAccess(Base):
+	__tablename__ = 'usercommandclasses'
+	id = Column(Integer, primary_key=True)
+	user_id = Column(Integer, ForeignKey('users.id'))
+	user = relationship(User, backref=backref('usercommandclasses', uselist=True))
+	squelch = Column(Boolean, nullable=False)
 
 class Command(Base):
 	__tablename__ = 'commands'
@@ -52,37 +80,6 @@ class Command(Base):
 	command_class_id = Column(Integer, ForeignKey('commandclasses.id'))
 	command_class = relationship(CommandClass, backref=backref('commands', uselist=True))
 
-class RoleCommandAccess(Base):
-	__tablename__ = 'rolecommands'
-	id = Column(Integer, primary_key=True)
-	role = Column(Integer, ForeignKey('roles.id'))
-	command_id = Column(Integer, ForeignKey('commands.id'))
-	command = relationship(Command, backref=backref('rolecommands', uselist=True))
-	squelch = Column(Boolean, nullable=False)
-
-class RoleCommandClassAccess(Base):
-	__tablename__ = 'rolecommandclasses'
-	id = Column(Integer, primary_key=True)
-	role = Column(Integer, ForeignKey('roles.id'))
-	command_class_id = Column(Integer, ForeignKey('commandclasses.id'))
-	command_class = relationship(CommandClass, backref=backref('commands', uselist=True))
-	squelch = Column(Boolean, nullable=False)
-
-class UserCommandAccess(Base):
-	__tablename__ = 'usercommands'
-	id = Column(Integer, primary_key=True)
-	user = Column(Integer, ForeignKey('users.id'))
-	command_id = Column(Integer, ForeignKey('commands.id'))
-	command = relationship(Command, backref=backref('rolecommands', uselist=True))
-	squelch = Column(Boolean, nullable=False)
-
-class UserCommandClassAccess(Base):
-	__tablename__ = 'usercommandclasses'
-	id = Column(Integer, primary_key=True)
-	user = Column(Integer, ForeignKey('users.id'))
-	command_class_id = Column(Integer, ForeignKey('commandclasses.id'))
-	command_class = relationship(CommandClass, backref=backref('commands', uselist=True))
-	squelch = Column(Boolean, nullable=False)
 
 
 
