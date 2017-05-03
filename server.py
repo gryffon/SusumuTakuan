2389c1c803938a9ec207a88e6c95930c477f1315
Gryffon <ryan@karetas.com>
2017-05-03 13:28:04 -0400

    Add owner to server initialization

--- server.py
+++ server.py
@@ -6,13 +6,21 @@
 
 import discord
 
-from database import Server, Channel
+from database import Server, Channel, User
 
 def register_server_and_channels(session, server):
+
+	owner_id = session.query(User).filter(User.id == server.owner.id).first()
+	if ( owner_id == None ):
+		the_owner = User(id=server.owner.id, name=server.owner.name)
+		session.add(the_owner)
+	else:
+		the_owner = owner_id
 
 	server_id = session.query(Server).filter(Server.name == server.name).first()
 	if ( server_id == None ):
 		the_server = Server(name=server.name)
+		the_server.owner = the_owner
 		session.add(the_server)
 	else:
 		the_server = server_id
