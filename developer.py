d7f5c1eb5f1696becacc702c73fa8309ab3d8b2c
Gryffon <ryan@karetas.com>
2017-05-11 17:55:21 -0400

    fix test statement

--- developer.py
+++ developer.py
@@ -57,7 +57,7 @@ def register_developer_access(session, developers):
 
 	for developer in developers:
 		the_developer = session.query(User).filter(User.id == developer).first()
-		if ( the_developer == None):
+		if ( the_developer != None):
 			the_developer.roles.append(developer_role)
 
 	session.commit()
