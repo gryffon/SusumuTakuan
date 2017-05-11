caeda34a8fc18c56cbbe46a549cb30f419a46cff
Gryffon <ryan@karetas.com>
2017-05-11 17:45:53 -0400

    Add additional imports

--- access.py
+++ access.py
@@ -4,7 +4,7 @@
 # functions for dealing with access to Discord bot commands
 #
 
-from database import CommandClassAccess
+from database import CommandClassAccess, User, Role
 
 def grant_user_access(user, commandclass):
 	new_grant = CommandClassAccess(user_id = user.id, command_class_id = commandclass.id)
