7a02f383986f347d208f69ba59526d9ce7df59bf
Gryffon <ryan@karetas.com>
2017-05-11 17:42:48 -0400

    Add access grant functions

---
+++ access.py
@@ -0,0 +1,17 @@
+#
+# access.py
+#
+# functions for dealing with access to Discord bot commands
+#
+
+def grant_user_access(user, commandclass):
+	new_grant = CommandClassAccess(user_id = user.id, command_class_id = commandclass.id)
+	session.add(new_grant)
+
+	session.commit()
+
+def grant_role_access(role, commandclass):
+	new_grant = CommandClassAccess(role_id = role.id, command_class_id = commandclass.id)
+	session.add(new_grant)
+
+	session.commit()
